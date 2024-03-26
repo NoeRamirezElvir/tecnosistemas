fcb1ca535caaafcf6676fa4373a64e4609e1b4f4
Gustavo <gustavo.tabora@ujcv.edu.hn>
2024-03-26 10:39:54 -0600

    Se agrego interfaz de usuario y css

---
+++ TECNOSISTEMAS/Migrations/20240326162153_migracionGustavo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TECNOSISTEMAS.Migrations
+{
+    public partial class migracionGustavo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
