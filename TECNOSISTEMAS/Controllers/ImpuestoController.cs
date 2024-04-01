477340b8e7c255ff256c7bc21f29fad91b6fd496
axel-pineda <axel.pineda@ujcv.edu.hn>
2024-04-01 10:55:06 -0600

    Creacion de crud Impuesto

---
+++ TECNOSISTEMAS/Controllers/ImpuestoController.cs
@@ -0,0 +1,132 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using TECNOSISTEMAS.Data;
+using TECNOSISTEMAS.Data.Entidades;
+using TECNOSISTEMAS.Filters;
+using TECNOSISTEMAS.Models;
+using TECNOSISTEMAS.Utilidades;
+namespace TECNOSISTEMAS.Controllers
+{
+    public class ImpuestoController : Controller
+    {
+        private readonly ILogger<ImpuestoController> _logger;
+        private TecnosistemasDbContext _context;
+
+        public ImpuestoController(ILogger<ImpuestoController> logger, TecnosistemasDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        //Principal
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult Index()
+        {
+            var impuesto = _context.Impuesto.Where(w => w.Eliminado == false).ProjectToType<ImpuestoVm>().ToList();
+            return View(impuesto);
+        }
+        //Insertar
+        [HttpGet]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult Insertar()
+        {
+            ImpuestoVm impuesto = new ImpuestoVm();
+            return View(impuesto); 
+        }
+        [HttpPost]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult Insertar(ImpuestoVm impuesto)
+        {
+            //obtener Usuario
+            var sesionBase64 = HttpContext.Session.GetString("UsuarioObjeto");
+            var base64EncodedBytes = System.Convert.FromBase64String(sesionBase64);
+            var sesion = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            UsuarioVm UsuarioObjeto = JsonConvert.DeserializeObject<UsuarioVm>(sesion);
+            //
+            if (!impuesto.Validacion())
+            {
+                TempData["mensaje"] = "Llene todos los campos";
+                return View(impuesto);
+            }
+            Impuesto nuevoRegistro = new Impuesto();
+            nuevoRegistro.Nombre = impuesto.Nombre;
+            nuevoRegistro.Valor = impuesto.Valor;
+
+            nuevoRegistro.Eliminado = false;
+            nuevoRegistro.CreatedDate = DateTime.Now;
+            nuevoRegistro.CreatedBy = UsuarioObjeto.Id;
+            nuevoRegistro.Id = Guid.NewGuid();
+            _context.Impuesto.Add(nuevoRegistro);
+            _context.SaveChanges();
+            TempData["mensaje"] = "Registro Exitoso";
+            return RedirectToAction("Index");
+        }
+        //Editar
+        [HttpGet]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult Editar(Guid ImpuestoId)
+        {
+            var impuesto = _context.Impuesto.Where(w => w.Id == ImpuestoId).ProjectToType<ImpuestoVm>().FirstOrDefault();
+            return View(impuesto);
+        }
+        [HttpPost]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult Editar(ImpuestoVm impuesto)
+        {
+            if (!impuesto.Validacion())
+            {
+                TempData["mensaje"] = "Llene todos los campos";
+                return View(impuesto);
+            }
+            var nuevoRegistro = _context.Impuesto.Where(w => w.Id == impuesto.Id).FirstOrDefault();
+            nuevoRegistro.Nombre = impuesto.Nombre;
+            nuevoRegistro.Valor = impuesto.Valor;
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "Actualizacion Exitosa";
+            return RedirectToAction("Index");
+        }
+        //Eliminar
+        [HttpGet]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult Eliminar(Guid ImpuestoId)
+        {
+            var impuesto = _context.Impuesto.Where(w => w.Id == ImpuestoId).ProjectToType<ImpuestoVm>().FirstOrDefault();
+            return View(impuesto);
+        }
+        [HttpPost]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult Eliminar(ImpuestoVm impuesto)
+        {
+            var nuevoRegistro = _context.Impuesto.Where(w => w.Id == impuesto.Id).FirstOrDefault();
+            nuevoRegistro.Eliminado = true;
+
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "Registro Eliminado";
+            return RedirectToAction("Index");
+        }
+        //Buscar Nombre
+        [HttpGet]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public IActionResult BuscarNombre()
+        {
+            return View(new ImpuestoVm());
+        }
+        [HttpPost]
+        [ClaimRequirement("Impuesto")] //Filtros
+        public JsonResult BuscarImpuestoNombre([FromForm] ImpuestoVm vm)
+        {
+            if (vm == null || string.IsNullOrEmpty(vm.Nombre))
+            {
+                var errorMessage = "Escriba el nombre a buscar";
+                var errorResult = AppResult.Error(errorMessage);
+                return new JsonResult(BadRequest(errorResult));
+            }
+            var impuesto = _context.Impuesto.Where(w => w.Eliminado == false && w.Nombre.ToLower() == vm.Nombre.ToLower()).ProjectToType<ImpuestoVm>().ToList();
+            var result = impuesto.Count == 0 ? AppResult.Error("No se encontro el impuesto deseado") : AppResult.Success("Impuesto encontrada", impuesto);
+            return new JsonResult(Ok(result));
+        }
+    }
+}
---
+++ TECNOSISTEMAS/Models/ImpuestoVm.cs
@@ -0,0 +1,21 @@
+﻿namespace TECNOSISTEMAS.Models
+{
+    public class ImpuestoVm
+    {
+        public string Nombre { set; get; }
+        public double Valor { set; get; }
+
+        //Obligatorios
+        public Guid Id { get; set; }
+        public bool Eliminado { set; get; }
+        public DateTime CreatedDate { set; get; }
+        public Guid CreatedBy { set; get; }
+
+        //Validaciones
+        public bool Validacion()
+        {
+            return true;
+
+        }
+    }
+}
